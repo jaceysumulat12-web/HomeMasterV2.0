526a081b1f779ca57cb9da8444c10409b90953f1
jaceysumulat12-web <jaceysumulat12@gmail.com>
2025-10-17 08:22:13 +0800

    Sql File

---
+++ homedb.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 15, 2025 at 09:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `homemasterdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product table`
+--
+
+CREATE TABLE `product table` (
+  `Code` int(11) NOT NULL,
+  `Product` text NOT NULL,
+  `Specificatios` text NOT NULL,
+  `Category` text NOT NULL,
+  `Stocks` int(11) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `Status` text NOT NULL,
+  `Actions` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product table`
+--
+ALTER TABLE `product table`
+  ADD PRIMARY KEY (`Code`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
